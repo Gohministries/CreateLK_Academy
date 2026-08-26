f442874ea68626c8bdbaf3331bc47b7aade34e01
Gohministries <gateofheavensl1@gmail.com>
2026-08-26 06:56:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,106 +1,150 @@
-const CACHE_NAME = "createlk-academy-v2";
+const CACHE_NAME = "createlk-academy-v4";
 
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./hero.png"
+const CORE_FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json"
 ];
 
 
-self.addEventListener("install", event => {
+self.addEventListener(
+  "install",
+  event => {
 
-  event.waitUntil(
+    event.waitUntil(
 
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
+      caches
+        .open(CACHE_NAME)
+        .then(cache => {
 
-  );
+          return cache.addAll(
+            CORE_FILES
+          );
 
-  self.skipWaiting();
+        })
 
-});
+    );
 
+    self.skipWaiting();
 
-self.addEventListener("activate", event => {
-
-  event.waitUntil(
-
-    caches.keys().then(keys => {
-
-      return Promise.all(
-
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-
-      );
-
-    })
-
-  );
-
-  self.clients.claim();
-
-});
-
-
-self.addEventListener("fetch", event => {
-
-  if(event.request.method !== "GET"){
-    return;
   }
+);
 
-  event.respondWith(
 
-    caches.match(event.request)
-      .then(cached => {
+self.addEventListener(
+  "activate",
+  event => {
 
-        if(cached){
-          return cached;
-        }
+    event.waitUntil(
 
-        return fetch(event.request)
+      caches
+        .keys()
+        .then(keys => {
+
+          return Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    if(
+      event.request.method !== "GET"
+    ){
+
+      return;
+
+    }
+
+
+    event.respondWith(
+
+      caches
+        .match(event.request)
+        .then(cachedResponse => {
+
+          if(cachedResponse){
+
+            return cachedResponse;
+
+          }
+
+
+          return fetch(
+            event.request
+          )
+
           .then(response => {
 
+            if(!response){
+
+              return response;
+
+            }
+
+
             if(
-              !response ||
-              response.status !== 200 ||
-              response.type === "opaque"
+              response.status !== 200 &&
+              response.type !== "opaque"
             ){
 
               return response;
 
             }
 
-            const copy =
-            response.clone();
 
-            caches.open(CACHE_NAME)
+            const responseClone =
+              response.clone();
+
+
+            caches
+              .open(CACHE_NAME)
               .then(cache => {
 
                 cache.put(
                   event.request,
-                  copy
+                  responseClone
                 );
 
               });
 
+
             return response;
 
           })
+
           .catch(() => {
 
             return caches.match(
-              "./index.html"
+              "/index.html"
             );
 
           });
 
-      })
+        })
 
-  );
+    );
 
-});
+  }
+);
